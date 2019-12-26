52bf99cc25eca06222c06d7252ec1880b5d769ab
Shikha <sbhatt10@uncc.edu>
2019-12-26 13:37:59 -0500

    Method one of defining key and removing warning message

--- src/screens/Components.js
+++ src/screens/Components.js
@@ -1,21 +1,17 @@
-import React from 'react'; //how different componenet work together
-import { Text, StyleSheet,View } from 'react-native'; //takes component and displays it
+import React from 'react';
+import { Text, StyleSheet,View } from 'react-native';
 import { paddedString } from 'uuid-js';
 
-//use function or arrow =>
 const ComponentsScreen = function (){
-   // const greeting = 'Hi there'; //you cant display javascript object inside text object.
-    const fname = <Text>Shikha</Text> // use when you have conditional jsx
+    const fname = <Text>Shikha</Text>
     return(<View>
         <Text style={styles.headerStyle}>Getting started with React Native!</Text>
         <Text style={styles.textStyle} >My name is {fname}</Text>
     </View>
     );
-    //return some JSX (what we want to display in our app)
 }; 
 
 const styles = StyleSheet.create({
-    //name anything you want
     headerStyle: {
         fontSize: 45
     },
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
-
+//Flatlist with key, key should be unique
 const ListScreen = () => {
-    return <Text>List Screen</Text>
+    const grocery = [
+        {name: 'Apple', key: '1'}, 
+        {name: 'Orange', key: '2'},
+        {name: 'Tissue', key: '3'},
+        {name: 'Milk', key:'4'},
+        {name: 'Tea', key:'5'},
+    ];
+//Working with flatlist    
+//Define dataprop - array of data used to create elements out of
+//Define renderItem prop - fn that will turn each individual item into an eleemnt
+//Define keyExtractor instead of providing key for individual list. 
+
+return (
+    <FlatList 
+        data={grocery} 
+        renderItem={({item})=> { //item: listobj
+        return <Text>{item.name}</Text>
+        }} 
+    />
+    );
 };
 
 const styles = StyleSheet.create({});
