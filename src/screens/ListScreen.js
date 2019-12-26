b59cc7f2b719f174a1db795c17bb7bb7c72be680
Shikha <sbhatt10@uncc.edu>
2019-12-26 13:41:08 -0500

    Method two of defining key usinng keyExtractor and removing warning message

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
-//Flatlist with key, key should be unique
 const ListScreen = () => {
     const grocery = [
-        {name: 'Apple', key: '1'}, 
-        {name: 'Orange', key: '2'},
-        {name: 'Tissue', key: '3'},
-        {name: 'Milk', key:'4'},
-        {name: 'Tea', key:'5'},
+        {name: 'Apple'},
+        {name: 'Orange'},
+        {name: 'Tissue'},
+        {name: 'Milk'},
+        {name: 'Tea'},
     ];
 //Working with flatlist    
 //Define dataprop - array of data used to create elements out of
 //Define renderItem prop - fn that will turn each individual item into an eleemnt
-//Define keyExtractor instead of providing key for individual list. 
 
 return (
     <FlatList 
+        //keyextractor using name as unique key
+        keyExtractor = {(grocery) => grocery.name}
         data={grocery} 
         renderItem={({item})=> { //item: listobj
         return <Text>{item.name}</Text>
